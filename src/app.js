a30933531b47364ec0bff2361d5d38064dc3c693
Jorge Barbosa <jorgeluis6@gmail.com>
2020-05-25 15:41:31 -0700

    Implementing login and logout and redirection (part1)

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './routes/AppRouter';
+import AppRouter, { history } from './routes/AppRouter';
 import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 import { startSetExpenses } from './actions/expenses';
 
 const store = configureStore();
@@ -16,12 +16,28 @@ const jsx = (
     <Provider store={store}>
         <AppRouter />
     </Provider>
-)
+);
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
+let hasRendered = false;
+const renderApp = () => {
+    if (!hasRendered) {
+        ReactDOM.render(jsx, document.getElementById('app'));
+        hasRendered = true;
+    }
+};
 
-store.dispatch(startSetExpenses()).then(() => {    
-    ReactDOM.render(jsx, document.getElementById('app'))
-})
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp();
+            if (history.location.pathname === '/') {
+                history.push('/dashboard')
+            }
+        });
+    } else {
+        renderApp();
+        history.push('/')
+    }
+});
